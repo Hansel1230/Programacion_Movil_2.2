5706005a392288b1b988e9336fdc6fdfab0263f3
HdelossantosRamirez <hdelossantos@dextra.com.do>
2025-09-30 11:45:47 -0400

    Create Layout

--- Partial1_Apps/About_Me/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Partial1_Apps/About_Me/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -12,6 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aboutme.ui.theme.AboutMeTheme
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +33,21 @@ class MainActivity : ComponentActivity() {
             }
         }
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.done_button).setOnClickListener {
+            addNickName(it)}
+    }
+    private fun addNickName(view: View)
+    {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
 
